05029439d6298fd829e8718fdf86feb13da073dc
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-09-13 17:15:29 -0400

    Remove fmt (unused import)

--- test/integration/mysql/mysql_test.go
+++ test/integration/mysql/mysql_test.go
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package simple_example
+package mysql
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
