9320312f85aa99a2865a31ebe9f159bf6cbdd437
Nim Jayawardena <i.am.nim.jay@gmail.com>
2025-05-12 15:03:38 -0400

    fix: Remove <= constraint on google TF provider

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "<= 4.74, != 4.75.0"
+      version = "!= 4.75.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "<= 4.74, != 4.75.0"
+      version = "!= 4.75.0"
     }
     random = {
       source  = "hashicorp/random"
